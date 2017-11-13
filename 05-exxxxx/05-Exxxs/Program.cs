cd13d2f864684320854aa781c0239aa159744863
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-13 14:14:26 +0200

    koolis

---
+++ 05-exxxxx/05-Exxxs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Exxxs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+;            for (int n = 1; n < 6; n++)
+            {
+                Console.Write("{0}\t",n);
+                for (int m = 1; m < 6; m++)
+                {
+                    Console.Write("{0}\t",n * m);
+
+                }
+                Console.WriteLine("");
+
+
+            }
+                Console.ReadLine();
+            
+        }
+    }
+}
