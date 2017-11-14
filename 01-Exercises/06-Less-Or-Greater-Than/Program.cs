595ff64466a99e684b7d8df597341a62f413794d
Mikk Lagemaa <mikk.lagemaa@gmail.com>
2017-11-14 23:34:20 +0200

    commit otse visualist?

--- 01-Exercises/06-Less-Or-Greater-Than/Program.cs
+++ 01-Exercises/06-Less-Or-Greater-Than/Program.cs
@@ -10,6 +10,27 @@ namespace _06_Less_Or_Greater_Than
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kirjuta üks number ning ütlen kas su number on suurem või väiksem kui minu oma.");
+            Console.Write("Sisesta enda number: ");
+            string num = Console.ReadLine();
+            int nuum = int.Parse(num);
+
+            int arv = 34;
+
+            if (arv > nuum)
+            {
+                Console.WriteLine("Sinu number on väiksem kui minu oma.");
+            }
+            if (arv < nuum) 
+            {
+                Console.WriteLine("Sinu number on suurem kui minu oma.");
+            }
+            if (arv == nuum)
+            {
+                Console.WriteLine("Arvasid minu numbri. Väga ootamatu!");
+            }
+            Console.WriteLine("Press Enter...");
+            Console.ReadLine();
         }
     }
 }
