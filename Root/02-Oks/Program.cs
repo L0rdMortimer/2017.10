2453256f8fdbfa1919d937707a9c2936bf7e624e
Mikk Lagemaa <Mikk.Lagemaa@tptlive.ee>
2018-01-15 14:14:40 +0200

    Root

---
+++ Root/02-Oks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Oks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
