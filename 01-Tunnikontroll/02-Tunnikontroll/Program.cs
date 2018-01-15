20c1b9a1f8333f9f6acb9a55b2cf59e266570ce1
L0rdMortimer <33531910+L0rdMortimer@users.noreply.github.com>
2018-01-15 13:32:32 +0200

    Add files via upload

---
+++ 01-Tunnikontroll/02-Tunnikontroll/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Tunnikontroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Toidu Kodu");
+            Console.WriteLine("Kõrvitsa 2, Harjumaa");
+            Console.WriteLine("*******************************************");
+            while (true)
+            {
+                Console.Write("Enter price of food item [-1 to quit]:");
+                string sisend = Console.ReadLine();
+                
+                if (sisend == "-1") break;
+
+                { summa= summa+sisend
+            }
+            Console.WriteLine("*******************************************");
+
+            Console.WriteLine("Subtotal: ");
+            //Console.WriteLine("15.00 % Gratuity: €2,53");
+            //Console.WriteLine("Total: €14,35");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ 01-Tunnikontroll/03-Tunnitöö/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Tunnitöö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Taco Palenque");
+            Console.WriteLine("1200 Main ST:");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("Price of food item: 7,99");
+            Console.WriteLine("Price of food item: 6,55");
+            Console.WriteLine("Price of food item: 2,345");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("Subtotal: €16,88");
+            Console.WriteLine("15.00% Gratuity:: €2,53");
+            Console.WriteLine("Total: €14.35");
+            Console.ReadLine();
+        }
+    }
+}
